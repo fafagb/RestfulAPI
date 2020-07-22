193f05516503f07fa0d3d37f932875d8b1703f7e
fafagb <fafagb2009@aliyun.com>
2020-07-22 10:10:54 +0800

    update files

--- API/Program.cs
+++ API/Program.cs
@@ -12,28 +12,23 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 
-namespace API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-               .MinimumLevel.Debug() //设置输出日志的最小级别
-               .MinimumLevel.Override("Microsoft", LogEventLevel.Information)//命名空间以Microsoft开头的日志输出的最小级别设置为Information
-               .Enrich.FromLogContext()
-               .WriteTo.Console()
-               .WriteTo.File(Path.Combine("logs", "api.txt"), rollingInterval: RollingInterval.Day)
-               .CreateLogger();
+namespace API {
+    public class Program {
+        public static void Main (string[] args) {
+            Log.Logger = new LoggerConfiguration ()
+                .MinimumLevel.Debug () //设置输出日志的最小级别
+                .MinimumLevel.Override ("Microsoft", LogEventLevel.Information) //命名空间以Microsoft开头的日志输出的最小级别设置为Information
+                .Enrich.FromLogContext ()
+                .WriteTo.Console ()
+                .WriteTo.File (Path.Combine ("logs", "api.txt"), rollingInterval : RollingInterval.Day)
+                .CreateLogger ();
 
-             CreateWebHostBuilder(args).Build().Run();
-     
-
+            CreateWebHostBuilder (args).Build ().Run ();
 
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)//
-                .UseStartup<Startup>().UseSerilog();//覆盖原有的log操作
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args) //
+            .UseStartup<Startup> ().UseSerilog (); //覆盖原有的log操作
     }
 }
--- Study.md
+++ Study.md
@@ -1,4 +1,3 @@
-# Update
 # Common shortcut keys
 # ctrl+p  search files       ctrl+shift+o   搜索属性方法
 
